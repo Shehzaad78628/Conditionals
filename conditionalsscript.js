f46068db8a38c1ace1483a1ee151ee59a72fe922
Shehzaad Nasir <shehzaadnasir786@gmail.com>
2024-02-09 14:10:10 -0500

    git remote add origin https://github.com/Shehzaad78628/Conditionals.git git branch -M main git push -u origin main

---
+++ conditionalsscript.js
@@ -0,0 +1,12 @@
+// alert ("You guessed right! The coin flip landed on [YOUR CHOICE HERE]")
+// alert ("Sorry, the coin flip landed on [NOT YOUR CHOICE HERE]")
+var userChoice = "heads or tails?";
+var randomNumber = "random number generator";
+var computerChoice = "assign value of heads or tails to computer choice";
+
+
+
+alert("You guessed right! The coin flip landed on [YOUR CHOICE HERE]");
+alert("Sorry, the coin flip landed on [NOT YOUR CHOICE HERE]");
+ 
+
